e08bbd2bf6ec4d6cc4ba5ecfed547157b94295a3
Daniel Ireson <danielireson@gmail.com>
2021-03-05 16:08:50 +0000

    feat: add fixed home layout

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,23 @@
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import "./Home.css";
 
 function Home() {
+  const history = useHistory();
+
+  function handleCreateTimer() {
+    history.push("/a7s6d");
+  }
+
   return (
     <div className="Home">
-      <header className="Home-header">
-        <p>
-          Go <Link to="/timer">/timer</Link>
-        </p>
-      </header>
+      <h1 className="Home-title">Create new timer</h1>
+      <div className="Home-shortcode-input">
+        <span>http://localhost/</span>
+        <input type="text" id="shortcode" defaultValue="a7s6d" size="5" />
+      </div>
+      <button className="Home-button" onClick={handleCreateTimer}>
+        Create timer
+      </button>
     </div>
   );
 }

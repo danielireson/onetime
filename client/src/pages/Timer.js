e70f103595aef55f59c91136087b27c4e2751e0d
Daniel Ireson <danielireson@gmail.com>
2021-06-30 17:12:46 +0100

    feat: show "greater than" for distant times

--- client/src/pages/Timer.js
+++ client/src/pages/Timer.js
@@ -6,6 +6,8 @@ import useTimerApi from "../hooks/useTimerApi";
 import { friendlyTime } from "../utils/time";
 import styles from "./Timer.module.css";
 
+const MAX_DISPLAY_MINUTES = 1440;
+
 function Timer() {
   const TIMER_URL = window.location.href;
   const history = useHistory();
@@ -13,6 +15,7 @@ function Timer() {
   const { endTime, updateEndTime } = useTimerApi(timerId);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
+  const showMinutes = minutes < MAX_DISPLAY_MINUTES;
   const showSeconds = minutes.toString().length < 4;
 
   useEffect(() => {
@@ -46,10 +49,18 @@ function Timer() {
       <CloseButton onClose={navigateHome} />
       <h2 className={styles.url}>{TIMER_URL}</h2>
       <h1 className={styles.time}>
-        <span className={styles.period}>
-          {minutes}
-          <span className={styles.unit}>m</span>
-        </span>
+        {!showMinutes && (
+          <span className={styles.period}>
+            &gt;{MAX_DISPLAY_MINUTES}
+            <span className={styles.unit}>m</span>
+          </span>
+        )}
+        {showMinutes && (
+          <span className={styles.period}>
+            {minutes}
+            <span className={styles.unit}>m</span>
+          </span>
+        )}
         {showSeconds && (
           <span className={styles.period}>
             {seconds}

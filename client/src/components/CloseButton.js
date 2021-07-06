970a8f54069367e4b7ce4c2c3e2bd7bec0239094
Daniel Ireson <danielireson@gmail.com>
2021-07-06 16:01:13 +0100

    refactor: extract icon component

--- client/src/components/CloseButton.js
+++ client/src/components/CloseButton.js
@@ -1,3 +1,4 @@
+import Icon from "./Icon";
 import styles from "./CloseButton.module.css";
 
 function CloseButton({ showButton, onClick }) {
@@ -7,12 +8,7 @@ function CloseButton({ showButton, onClick }) {
 
   return (
     <button className={styles.button} onClick={onClick}>
-      <svg className={styles.svg} viewBox="0 0 24 24">
-        <path
-          fill="currentColor"
-          d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
-        />
-      </svg>
+      <Icon className={styles.icon} name="close" />
     </button>
   );
 }

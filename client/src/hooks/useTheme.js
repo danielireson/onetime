8a450bed629bd04201cbdfd7b75ccccc2b641177
Daniel Ireson <danielireson@gmail.com>
2021-07-20 14:15:39 +0100

    refactor: throw error for invalid context usage

--- client/src/hooks/useTheme.js
+++ client/src/hooks/useTheme.js
@@ -2,7 +2,13 @@ import { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 export default function useTheme() {
-  const { theme, toggleTheme } = useContext(ThemeContext);
+  const context = useContext(ThemeContext);
+
+  if (context === undefined) {
+    throw new Error("'useTheme' must be used within a 'ThemeProvider'");
+  }
+
+  const { theme, toggleTheme } = context;
 
   const isDarkTheme = theme === "dark";
   const isLightTheme = theme === "light";
